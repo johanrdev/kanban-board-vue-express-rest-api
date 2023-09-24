cc2869b79ea728eb1d41e6111d2835893dee1293
johanrdev <johanrdev2@gmail.com>
2023-09-24 08:10:45 +0200

    Base component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template lang="html">
   <section class="max-w-5xl mx-auto mt-24 mb-12 grid md:grid-cols-3 gap-4">
-    <div class="border p-2">
-      <h2 class="text-xl mb-2">Todo (0)</h2>
-      <ul>
-        <li v-for="index in 5" :key="index"
-          class="p-2 mb-1 last:mb-0 min-h-[80px] border border-l-8 border-l-indigo-500 rounded">Item {{ index }}</li>
-      </ul>
-    </div>
-    <div class="border p-2">
-      <h2 class="text-xl mb-2">Progress (0)</h2>
-      <ul>
-        <li v-for="index in 5" :key="index"
-          class="p-2 mb-1 last:mb-0 min-h-[80px] border border-l-8 border-l-rose-500 rounded">Item {{ 5 + index }}</li>
-      </ul>
-    </div>
-    <div class="border p-2">
-      <h2 class="text-xl mb-2">Complete (0)</h2>
-      <ul>
-        <li v-for="index in 5" :key="index"
-          class="p-2 mb-1 last:mb-0 min-h-[80px] border border-l-8 border-l-emerald-500 rounded">Item {{ 10 + index }}</li>
-      </ul>
-    </div>
+    <KanbanColumn name="Todo" color="indigo" />
+    <KanbanColumn name="In Progress" color="rose" />
+    <KanbanColumn name="Complete" color="emerald" />
   </section>
 </template>
 <script>
+import KanbanColumn from './components/KanbanColumn.vue'
+
 export default {
+  components: {
+    KanbanColumn
+  },
   setup() {
 
   }
---
+++ src/components/KanbanColumn.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <section class="border p-2">
+    <h2 class="text-xl mb-2" v-if="name">{{ name }}</h2>
+
+    <ul>
+      <li v-for="index in 5" :key="index" class="min-h-[80px] border border-l-8 rounded p-2 mb-1 last:mb-0">Item {{ index }}</li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  props: ['name', 'color']
+}
+</script>
+<style lang="css">
+  
+</style>
