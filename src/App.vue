33629207b0d72468b27fa97fb5267a35a30047a1
johanrdev <johanrdev2@gmail.com>
2023-09-24 07:45:38 +0200

    Responsive grid

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template lang="html">
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <section class="max-w-5xl mx-auto mt-24 mb-12 grid md:grid-cols-3 gap-4">
+    <div class="border">
+      <h2>Todo</h2>
+    </div>
+    <div class="border">
+      <h2>In Progress</h2>  
+    </div>
+    <div class="border">
+      <h2>Complete</h2>
+    </div>
+  </section>
 </template>
 <script>
 export default {
   setup() {
-    
+
   }
 }
 </script>
-<style lang="css">
-  
-</style>
+<style lang="css"></style>
