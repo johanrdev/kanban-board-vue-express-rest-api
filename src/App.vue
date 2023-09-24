2a51eda4cd485b1a970b3be11df44c038947e2e7
johanrdev <johanrdev2@gmail.com>
2023-09-24 09:44:00 +0200

    Create column objects and add stagger transitions

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,10 @@
         <span class="block text-3xl md:text-4xl text-gray-300 text-center">VueKanbanBoard</span>
       </header>
       <section class="my-8 grid md:grid-cols-3 gap-4">
-        <KanbanColumn name="Todo" color="#6366f1" status="todo" :data="todo" @change-status="onStatusChanged" />
-        <KanbanColumn name="In Progress" color="#f43f5e" status="progress" :data="progress"
-          @change-status="onStatusChanged" />
-        <KanbanColumn name="Complete" color="#10b981" status="complete" :data="complete"
-          @change-status="onStatusChanged" />
+        <transition-group appear @before-enter="beforeEnter" @enter="enter" :css="false">
+          <KanbanColumn v-for="(column, index) in columns" :key="column.id" :name="column.name" :color="column.color"
+            :status="column.status" :data="column.data" :data-index="index" @change-status="onStatusChanged" />
+        </transition-group>
       </section>
     </div>
   </transition>
@@ -18,12 +17,36 @@
 import { computed, ref } from 'vue'
 import KanbanColumn from './components/KanbanColumn.vue'
 import { uuid } from 'vue-uuid'
+import gsap from 'gsap'
 
 export default {
   components: {
     KanbanColumn
   },
   setup() {
+    const columns = ref([
+      {
+        id: uuid.v1(),
+        name: 'Todo',
+        color: '#6366f1',
+        status: null,
+        data: computed(() => items.value.filter(i => i.status === null))
+      },
+      {
+        id: uuid.v1(),
+        name: 'In Progress',
+        color: '#f43f5e',
+        status: 'progress',
+        data: computed(() => items.value.filter(i => i.status === 'progress'))
+      },
+      {
+        id: uuid.v1(),
+        name: 'Complete',
+        color: '#10b981',
+        status: 'complete',
+        data: computed(() => items.value.filter(i => i.status === 'complete'))
+      }
+    ])
     const items = ref([
       { id: uuid.v1(), name: 'My first todo', status: null },
       { id: uuid.v1(), name: 'My second todo', status: null },
@@ -37,20 +60,29 @@ export default {
       { id: uuid.v1(), name: 'My tenth todo', status: null }
     ])
 
-    const todo = computed(() => items.value.filter(i => i.status === null))
-    const progress = computed(() => items.value.filter(i => i.status === 'progress'))
-    const complete = computed(() => items.value.filter(i => i.status === 'complete'))
-
     const onStatusChanged = (data) => {
       const item = items.value.find(i => i.id === data.id)
-      item.status = data.status === 'todo' ? null : data.status
+      item.status = data.status
+    }
+
+    const beforeEnter = (el) => {
+      el.style.opacity = 0
+    }
+
+    const enter = (el, done) => {
+      gsap.to(el, {
+        opacity: 1,
+        duration: 2,
+        delay: el.dataset.index * 0.5,
+        onComplete: done
+      })
     }
 
     return {
-      todo,
-      progress,
-      complete,
-      onStatusChanged
+      columns,
+      onStatusChanged,
+      beforeEnter,
+      enter
     }
   }
 }
