ccec24effb50a12f8e01ecaa2a67c0fdcd5d871c
johanrdev <johanrdev2@gmail.com>
2023-09-24 08:20:25 +0200

    Border left color style binding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <section class="max-w-5xl mx-auto mt-24 mb-12 grid md:grid-cols-3 gap-4">
-    <KanbanColumn name="Todo" color="indigo" />
-    <KanbanColumn name="In Progress" color="rose" />
-    <KanbanColumn name="Complete" color="emerald" />
+    <KanbanColumn name="Todo" color="#6366f1" />
+    <KanbanColumn name="In Progress" color="#f43f5e" />
+    <KanbanColumn name="Complete" color="#10b981" />
   </section>
 </template>
 <script>
--- src/components/KanbanColumn.vue
+++ src/components/KanbanColumn.vue
@@ -3,15 +3,16 @@
     <h2 class="text-xl mb-2" v-if="name">{{ name }}</h2>
 
     <ul>
-      <li v-for="index in 5" :key="index" class="min-h-[80px] border border-l-8 rounded p-2 mb-1 last:mb-0">Item {{ index }}</li>
+      <li v-for="index in 5" :key="index" class="min-h-[80px] border border-l-8 rounded p-2 mb-1 last:mb-0"
+        :style="{ borderLeftColor: color }">Item {{ index }}</li>
     </ul>
   </section>
 </template>
 <script>
 export default {
-  props: ['name', 'color']
+  props: ['name', 'color'],
+  setup(props) {
+  }
 }
 </script>
-<style lang="css">
-  
-</style>
+<style lang="css"></style>
