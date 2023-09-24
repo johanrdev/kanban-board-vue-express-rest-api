f4abd9fa386b039c98ccc7b8fdcf7c28e84c96cd
johanrdev <johanrdev2@gmail.com>
2023-09-24 07:52:01 +0200

    List items

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template lang="html">
   <section class="max-w-5xl mx-auto mt-24 mb-12 grid md:grid-cols-3 gap-4">
-    <div class="border">
-      <h2>Todo</h2>
+    <div class="border p-2">
+      <h2 class="text-xl mb-2">Todo (0)</h2>
+      <ul>
+        <li v-for="index in 5" :key="index"
+          class="p-2 mb-1 last:mb-0 min-h-[80px] border border-l-8 border-l-indigo-500 rounded">Item {{ index }}</li>
+      </ul>
     </div>
-    <div class="border">
-      <h2>In Progress</h2>  
+    <div class="border p-2">
+      <h2 class="text-xl mb-2">Progress (0)</h2>
+      <ul>
+        <li v-for="index in 5" :key="index"
+          class="p-2 mb-1 last:mb-0 min-h-[80px] border border-l-8 border-l-rose-500 rounded">Item {{ 5 + index }}</li>
+      </ul>
     </div>
-    <div class="border">
-      <h2>Complete</h2>
+    <div class="border p-2">
+      <h2 class="text-xl mb-2">Complete (0)</h2>
+      <ul>
+        <li v-for="index in 5" :key="index"
+          class="p-2 mb-1 last:mb-0 min-h-[80px] border border-l-8 border-l-emerald-500 rounded">Item {{ 10 + index }}</li>
+      </ul>
     </div>
   </section>
 </template>
