23bc89098c2651c07deb5aff8312f59c3e7d1b05
johanrdev <johanrdev2@gmail.com>
2023-09-24 08:30:35 +0200

    Define array of data and pass filtered arrays to columns

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template lang="html">
   <section class="max-w-5xl mx-auto mt-24 mb-12 grid md:grid-cols-3 gap-4">
-    <KanbanColumn name="Todo" color="#6366f1" />
-    <KanbanColumn name="In Progress" color="#f43f5e" />
-    <KanbanColumn name="Complete" color="#10b981" />
+    <KanbanColumn name="Todo" color="#6366f1" :data="todo" />
+    <KanbanColumn name="In Progress" color="#f43f5e" :data="progress" />
+    <KanbanColumn name="Complete" color="#10b981" :data="complete" />
   </section>
 </template>
 <script>
+import { computed, ref } from 'vue'
 import KanbanColumn from './components/KanbanColumn.vue'
 
 export default {
@@ -13,7 +14,28 @@ export default {
     KanbanColumn
   },
   setup() {
+    const data = ref([
+      { id: 0, name: 'My first todo', status: null },
+      { id: 1, name: 'My second todo', status: null },
+      { id: 2, name: 'My third todo', status: null },
+      { id: 3, name: 'My fourth todo', status: null },
+      { id: 4, name: 'My fifth todo', status: null }, 
+      { id: 5, name: 'My sixth todo', status: null },
+      { id: 6, name: 'My seventh todo', status: null },
+      { id: 7, name: 'My eighth todo', status: null },
+      { id: 8, name: 'My ninth todo', status: null },
+      { id: 9, name: 'My tenth todo', status: null }
+    ])
 
+    const todo = computed(() => data.value.filter(i => i.status === null))
+    const progress = computed(() => data.value.filter(i => i.status === 'progress'))
+    const complete = computed(() => data.value.filter(i => i.status === 'complete'))
+
+    return {
+      todo,
+      progress,
+      complete
+    }
   }
 }
 </script>
--- src/components/KanbanColumn.vue
+++ src/components/KanbanColumn.vue
@@ -3,15 +3,17 @@
     <h2 class="text-xl mb-2" v-if="name">{{ name }}</h2>
 
     <ul>
-      <li v-for="index in 5" :key="index" class="min-h-[80px] border border-l-8 rounded p-2 mb-1 last:mb-0"
-        :style="{ borderLeftColor: color }">Item {{ index }}</li>
+      <li v-for="item in data" :key="item.id" class="min-h-[80px] border border-l-8 rounded p-2 mb-1 last:mb-0"
+        :style="{ borderLeftColor: color }" v-if="data.length">{{ item.name }}</li>
+      <li class="border border-l-8 rounded p-2 mb-1 last:mb-0" v-else>No data</li>
     </ul>
   </section>
 </template>
 <script>
 export default {
-  props: ['name', 'color'],
+  props: ['name', 'color', 'data'],
   setup(props) {
+    console.log(props.data)
   }
 }
 </script>
