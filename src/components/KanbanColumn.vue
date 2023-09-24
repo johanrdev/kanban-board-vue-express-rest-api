10e1943245d81fe78399f696604535c70aa6411c
johanrdev <johanrdev2@gmail.com>
2023-09-24 08:48:06 +0200

    Display item ID

--- src/components/KanbanColumn.vue
+++ src/components/KanbanColumn.vue
@@ -3,8 +3,12 @@
     <h2 class="text-xl mb-2" v-if="name">{{ name }} ({{ data.length }})</h2>
 
     <ul @drop="onDrop($event)" @dragenter.prevent @dragover.prevent>
-      <li v-for="item in data" :key="item.id" class="border border-l-8 rounded p-2 pb-8 mb-1 last:mb-0 cursor-move select-none"
-        :style="{ borderLeftColor: color }" v-if="data.length" draggable="true" @dragstart="startDrag($event, item)">{{ item.name }}</li>
+      <li v-for="item in data" :key="item.id"
+        class="border border-l-8 rounded flex flex-col p-2 mb-1 last:mb-0 cursor-move select-none"
+        :style="{ borderLeftColor: color }" v-if="data.length" draggable="true" @dragstart="startDrag($event, item)">
+        <span class="mb-8">{{ item.name }}</span>
+        <span class="text-xs text-right text-gray-400">{{ item.id }}</span>
+      </li>
       <li class="border border-l-8 rounded p-2 mb-1 last:mb-0" v-else>No data</li>
     </ul>
   </section>
@@ -21,7 +25,7 @@ export default {
 
     const onDrop = (event) => {
       const id = event.dataTransfer.getData('id')
-      console.log('id:',id)
+      console.log('id:', id)
     }
 
     return {
