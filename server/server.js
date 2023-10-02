d0d86c388330358e3c45b5ae4fcad28637e0adb7
johanrdev <johanrdev2@gmail.com>
2023-10-02 21:23:52 +0200

    Get all todo items from API and display them on the frontend

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const db = require('./database')
+const cors = require('cors')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 
 require('./database/connect')(db)
 require('./routes/todo.routes')(app)
