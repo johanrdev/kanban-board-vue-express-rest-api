385f02d1fda688aac5d198db5cf2e15204dcd84f
johanrdev <johanrdev2@gmail.com>
2023-10-02 21:07:36 +0200

    Clean up

--- server/server.js
+++ server/server.js
@@ -8,6 +8,5 @@ app.use(express.json())
 
 require('./database/connect')(db)
 require('./routes/todo.routes')(app)
-app.get('/', (req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
